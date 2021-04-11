95bd813ad61bad807137785eab8e640153b3d5f1
Kim <ntkimngan0705@gmail.com>
2021-04-11 19:16:48 +0700

    delete clg

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 app.get("/",(req,res)=>{
-    console.log("cicd");
     return res.send("hello cicd");
 })
 app.listen(3000,console.log("connect at port 3000"))
